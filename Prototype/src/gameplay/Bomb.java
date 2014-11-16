d5ee168c176dfb982d92b71d3721f16b11bdb793
Leonardo Siracusa <leonardo.siracusa@mail.mcgill.ca>
2014-11-15 21:17:00 -0500

    Added Bomb class

---
+++ Prototype/src/gameplay/Bomb.java
@@ -0,0 +1,18 @@
+package gameplay;
+
+public class Bomb {
+	private int damage;
+	private int range;
+	private int counter;
+	private int posX;
+	private int posY;
+	
+	public Bomb(){
+		
+	}
+	
+	public void explode(){
+		
+	}
+}
+
