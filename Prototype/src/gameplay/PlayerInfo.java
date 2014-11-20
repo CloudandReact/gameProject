2aed79636051f47ad497e6f8addbbcfcb0bb353e
leosiracusa <leonardo.siracusa@mail.mcgill.ca>
2014-11-19 20:24:46 -0500

    PlayerInfo Class

---
+++ Prototype/src/gameplay/PlayerInfo.java
@@ -0,0 +1,19 @@
+package gameplay;
+
+public class PlayerInfo {
+	
+	private static String username = "";
+	
+	public PlayerInfo(){
+		
+	}
+	
+	public static void setUsername(String x){
+		username = x;
+	}
+	
+	public static String getUsername(){
+		return username;
+	}
+	
+} 
