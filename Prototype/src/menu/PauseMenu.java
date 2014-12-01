06ab3c60650a7cdbde43331f7b821a45d6e4f087
rakinul <rakinul_huq@live.com>
2014-12-01 14:32:49 -0500

    Timer is probably fixed, couldnt check cause it doesn't work

--- Prototype/src/menu/PauseMenu.java
+++ Prototype/src/menu/PauseMenu.java
@@ -117,7 +117,7 @@ public class PauseMenu extends JFrame implements Serializable {
 			public void actionPerformed(ActionEvent e) {
 				// Execute when button is pressed
 				// getContentPane().removeAll();
-				
+				bomberman.stopTimer();
 				bomberman.destroyFrameTimer();
 				game.destroyPanel();
 				new MainMenu(panel, PlayerInfo.getUsername());
