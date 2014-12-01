e7ae8d5e040a837ed232ea4dfcaed50c220486c2
Leonardo Siracusa <leonardo.siracusa@mail.mcgill.ca>
2014-12-01 14:44:02 -0500

    Big changes

--- Prototype/src/gameplay/GameTimer.java
+++ Prototype/src/gameplay/GameTimer.java
@@ -21,7 +21,6 @@ public class GameTimer implements ActionListener, Serializable {
 	private JLabel livesLabel;
 	private JLabel scoreLabel;
 	private JLabel timeOverLabel;
-	private int score = 0;
 
 	/**
 	 * <p>
